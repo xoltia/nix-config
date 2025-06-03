fac27501aafab850e238aa4cb60a74cf06b1078f
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-06-03 18:45:18 -0500

    use home-manager for ghostty config

--- homeManagerModules/ghostty.nix
+++ homeManagerModules/ghostty.nix
@@ -1,16 +1,15 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ ghostty ];
-
-  home.file.".config/ghostty/config".text = ''
-    window-height = 30
-    window-width = 120
-    gtk-titlebar-hide-when-maximized = true
-    window-padding-balance = true
-    window-padding-color = extend
-    theme = Adwaita Dark
-    mouse-hide-while-typing = true
-  '';
+  programs.ghostty.enable = true;
+  programs.ghostty.settings = {
+    window-height = 30;
+    window-width = 120;
+    gtk-titlebar-hide-when-maximized = true;
+    window-padding-balance = true;
+    window-padding-color = "extend";
+    theme = "Adwaita Dark";
+    mouse-hide-while-typing = true;
+  };
 }
 
