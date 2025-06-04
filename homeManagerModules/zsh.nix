025dd7e09778456a61fde88a1b345cdc92ff07ab
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-06-03 20:27:28 -0500

    add starship

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -1,22 +1,51 @@
-{ ... }:
-
+{ lib, ... }:
 {
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = true;
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      format = lib.concatStrings [       
+        "$directory"
+        "$git_status"
+        "$jobs"
+        "$character"
+      ];
+      right_format = "$time";
+      add_newline = false;
+      directory = {
+        truncation_length = 4;
+        truncation_symbol = ".../";
+        truncate_to_repo = false;
+      };
+      jobs = {
+        format = "([\\[$symbol$number\\]]($style) )";
+        symbol = "+";
+        number_threshold = 1;
+      };
+      character = rec {
+        success_symbol = "[%](bold 250)";
+        error_symbol = success_symbol;
+      };
+      time = {
+        disabled = false;
+        style = "bold bright-black";
+        format = "[$time]($style)";
+      };
+    };
+  };
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable= true;
     syntaxHighlighting.enable = true;
-    shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos";
-    };
     history.size = 10000;
     initContent  = ''
       bindkey '^[[1;5C' forward-word
       bindkey '^[[1;5D' backward-word
-      BACKGROUND_JOBS="%(1j. %F{red}[%j]%f.)"
-      PS1="%K{blue}%n@%m%k %B%F{cyan}%~%f$BACKGROUND_JOBS%b %F{250}%%%f "
+      # BACKGROUND_JOBS="%(1j. %F{red}[%j]%f.)"
+      # PS1="%K{blue}%n@%m%k %B%F{cyan}%~%f$BACKGROUND_JOBS%b %F{250}%%%f "
     '';
   };
 }
