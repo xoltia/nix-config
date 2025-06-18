7349d0443195051b32c7a1cb2df4ee7f0a10bbee
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-06-18 00:02:29 -0500

    Disable NVIDIA S0ixPowerManagement to fix suspend

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -11,8 +11,11 @@
 
   # Maybe fixes occasional freezing.
   # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/739
+  # Also maybe fix suspend failing...
+  # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/472#issuecomment-2762633335
   boot.extraModprobeConfig = ''
     options nvidia NVreg_EnableGpuFirmware=0
+    options nvidia NVreg_EnableS0ixPowerManagement=0
   '';
 
   hardware.nvidia = {
