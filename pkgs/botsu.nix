0283a5f41938ec0641120de4ec9aedcd7492f4a8
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-06-19 22:57:51 +0000

    Update botsu

--- pkgs/botsu.nix
+++ pkgs/botsu.nix
@@ -2,12 +2,12 @@
 
 with pkgs; buildGoModule {
   pname = "botsu";
-  version = "0.3.4-dev.4263a2c";
+  version = "0.3.4-dev.0fc9fbc";
   src = fetchFromGitHub {
     owner = "xoltia";
     repo = "botsu";
-    rev = "4263a2cad704ce7f46e813054b662339f71c1b94";
-    hash = "sha256-N9/5vYuPY9is7prGZMDymtxD9J98jBHRWHAPa23EuyI=";
+    rev = "0fc9fbc62d5093ca004f6d218e79e060ef9368ec";
+    hash = "sha256-ceTqEbO6h/8NTNvW9Fne7QoK4ifGCp2/hi5eWIBigtI=";
   };
   subPackages = ["cmd/botsu"];
   vendorHash = "sha256-VZcR86ylVHI2jt4VdgvM8VA4iJtA6OM9ZhHF3tuG/vs=";
