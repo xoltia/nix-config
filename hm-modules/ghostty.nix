9a00a8b22f0e14d736c5ec4c2c424558e7e17be6
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-07-04 14:15:21 -0500

    Add ghostty keybinds

--- hm-modules/ghostty.nix
+++ hm-modules/ghostty.nix
@@ -1,15 +1,51 @@
 { pkgs, ... }:
 
 {
-  programs.ghostty.enable = true;
-  programs.ghostty.settings = {
-    window-height = 35;
-    window-width = 120;
-    gtk-titlebar-hide-when-maximized = true;
-    window-padding-balance = true;
-    window-padding-color = "extend";
-    theme = "Dark+";
-    mouse-hide-while-typing = true;
+  programs.ghostty = {
+    enable = true;
+    clearDefaultKeybinds = true;
+    settings = {
+      gtk-titlebar-hide-when-maximized = true;
+      mouse-hide-while-typing = true;
+      theme = "Dark+";
+      window-height = 35;
+      window-width = 120;
+      window-padding-balance = true;
+      window-padding-color = "extend";
+      keybind = [
+        # Tab creation and deletion
+        "ctrl+shift+t=new_tab"
+        "ctrl+shift+w=close_tab"
+
+        # Split resizing (ctrl + shift + <direction>)
+        "ctrl+shift+up=resize_split:up,20"
+        "ctrl+shift+down=resize_split:down,20"
+        "ctrl+shift+right=resize_split:right,20"
+        "ctrl+shift+left=resize_split:left,20"
+
+        # General movements and splits (ctrl+g trigger)
+        "ctrl+g>n=next_tab"
+        "ctrl+g>p=previous_tab"
+        "ctrl+g>v=new_split:right"
+        "ctrl+g>s=new_split:down"
+        "ctrl+g>h=goto_split:left"
+        "ctrl+g>j=goto_split:down"
+        "ctrl+g>k=goto_split:up"
+        "ctrl+g>l=goto_split:right"
+        "ctrl+g>q=close_surface"
+
+        # Indexed tab switching
+        "alt+1=goto_tab:1"
+        "alt+2=goto_tab:2"
+        "alt+3=goto_tab:3"
+        "alt+4=goto_tab:4"
+        "alt+5=goto_tab:5"
+        "alt+6=goto_tab:6"
+        "alt+7=goto_tab:7"
+        "alt+8=goto_tab:8"
+        "alt+9=goto_tab:9"
+      ];
+    };
   };
 }
 
