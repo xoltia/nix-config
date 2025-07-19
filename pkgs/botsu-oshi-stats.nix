b96713325de4e5ad58a0a101774a88dce7e1592f
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-07-19 20:34:55 +0000

    Update oshistats one more time

--- pkgs/botsu-oshi-stats.nix
+++ pkgs/botsu-oshi-stats.nix
@@ -2,12 +2,12 @@
 
 with pkgs; buildGoModule {
   pname = "botsu";
-  version = "6b4c945";
+  version = "1caaa11";
   src = fetchFromGitHub {
     owner = "xoltia";
     repo = "botsu-oshi-stats";
-    rev = "e8fb17f240221f0cd61d1a734ad5f00a8f2f3c5f";
-    hash = "sha256-xNgJhS1JcUiM9pP4Tw5Fke07YCXUr0tR9hecWM3Zr6I=";
+    rev = "1caaa11d2197fe21c6b14ef974de471cc14eceb0";
+    hash = "sha256-5fhum/nmqggjrVGg/jbvA9QGzsun2k0cX0hB80+b1vM=";
   };
   subPackages = [ "cmd/indexer" "cmd/server" "cmd/updater" ];
   vendorHash = "sha256-3qZ51QI4imOUpvGAczSfTNzq9Y/Bo8ogyJqiyaovNOg=";
