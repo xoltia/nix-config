7c139b0abdad02867dd88bf3e1c6ac5f39c9e397
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2025-08-03 22:24:59 -0500

    Nushell git in prompt and plugins

--- hm-modules/nushell.nix
+++ hm-modules/nushell.nix
@@ -1,7 +1,44 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.nushell = {
     enable = true;
+    plugins = with pkgs.nushellPlugins; [
+      polars
+      formats
+      gstat
+      query
+    ];
+    extraConfig = /*nu*/ ''
+      $env.PROMPT_COMMAND = {||
+          let dir = match (do -i { $env.PWD | path relative-to $nu.home-path }) {
+              null => $env.PWD
+              "" => '~'
+              $relative_pwd => ([~ $relative_pwd] | path join)
+          }
+
+          let path_color = (if (is-admin) { ansi red_bold } else { ansi green_bold })
+          let separator_color = (if (is-admin) { ansi light_red_bold } else { ansi light_green_bold })
+          let path_segment = $"($path_color)($dir)(ansi reset)" | str replace --all (char path_sep) $"($separator_color)(char path_sep)($path_color)"
+
+          let git_info = do -i {
+              let stats = gstat
+              if ($stats.repo_name != "no_repository") {
+                  let branch = $stats.branch
+                  let cols = $stats | transpose
+                  let dirty_idx = if (($cols | where ($it.column0 | str starts-with "idx_") | get column1 | math sum) > 0) { "+" } else { "" }
+                  let dirty_wt = if (($cols | where ($it.column0 | str starts-with "wt_") | get column1 | math sum) > 0) { "*" } else { "" }
+                  let ahead = if ($stats.ahead > 0) { "↑" } else { "" }
+                  let behind = if ($stats.behind > 0) { "↓" } else { "" }
+                  let stashes = if ($stats.stashes > 0) { "$" } else { "" }
+                  let status = $"($dirty_idx)($dirty_wt)($stashes)($ahead)($behind)"
+                  let status_block = if (($status | str length) > 1) { $"[($status)]" } else { $status }
+                  $" (ansi blue_bold)($branch)($status_block)(ansi reset)"
+              } else { "" }
+          }
+
+          $"($path_segment)($git_info)"
+      }
+    '';
   };
 
   programs.carapace = {
