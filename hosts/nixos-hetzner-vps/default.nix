0100b29a51d2ec46123d12d7cf874ae1ac18dc66
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2026-03-03 23:33:04 +0000

    Increase nginx body size limit for gokapi

--- hosts/nixos-hetzner-vps/default.nix
+++ hosts/nixos-hetzner-vps/default.nix
@@ -146,6 +146,9 @@
       forceSSL = true;
       enableACME = true;
       locations."/".proxyPass = "http://127.0.0.1:53842";
+      extraConfig = ''
+        client_max_body_size 100M;
+      '';
     };
 
     virtualHosts."imgproxy.jllamas.dev" = {
