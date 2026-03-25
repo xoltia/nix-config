79fdfdf4a01027b347f325242b5cc2a73772da50
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2026-03-24 19:33:55 -0500

    Add protonup-qt

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,5 +3,5 @@
   programs.steam.enable = true;
   programs.steam.extraCompatPackages = [ pkgs.proton-ge-bin ];
   programs.gamemode.enable = true;
-  environment.systemPackages = [ pkgs.r2modman ];
+  environment.systemPackages = [ pkgs.r2modman pkgs.protonup-qt ];
 }
